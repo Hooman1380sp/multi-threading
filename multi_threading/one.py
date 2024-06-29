09651cfc5a55672be5b0d05e1d4a6447aa051012
Hooman.sp <1380hz1380hz@gmail.com>
2024-06-29 16:21:42 +0330

    try semaphore and Timer in multi threading

--- multi_threading/one.py
+++ multi_threading/one.py
@@ -104,3 +104,65 @@ from threading import Thread, active_count, current_thread, enumerate
 # thread2.join()
 #
 # print(num)
+
+
+# Semaphore and BoundedSemaphore
+
+# from threading import BoundedSemaphore, Semaphore
+#
+# num = 0
+#
+# lock = Semaphore(3)
+#
+#
+# def add():
+#     global num
+#     lock.acquire()
+#     sleep(2)
+#     num += 1
+#     print(current_thread().getName())
+#     lock.release()
+#
+#
+# t1 = Thread(target=add)
+# t2 = Thread(target=add)
+# t3 = Thread(target=add)
+# t4 = Thread(target=add)
+# t5 = Thread(target=add)
+# t6 = Thread(target=add)
+# t7 = Thread(target=add)
+# t8 = Thread(target=add)
+# t9 = Thread(target=add)
+#
+# t1.start()
+# t2.start()
+# t3.start()
+# t4.start()
+# t5.start()
+# t6.start()
+# t7.start()
+# t8.start()
+# t9.start()
+#
+# t1.join()
+# t2.join()
+# t3.join()
+# t4.join()
+# t5.join()
+# t6.join()
+# t7.join()
+# t8.join()
+# t9.join()
+
+
+# timer
+# from threading import Timer
+#
+#
+# def show(obj_one):
+#     print(f"{obj_one} is a your title object")
+#
+#
+# t1 = Timer(interval=10, function=show, args=("blue",))
+#
+# t1.start()
